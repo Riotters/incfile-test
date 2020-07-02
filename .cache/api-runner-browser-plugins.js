9e38e4d38791ff113be2241a57f4b86ce3cadcd4
Riotters <krzysztof@riotters.com>
2020-07-02 13:39:02 +0200

    in progress

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-manifest/gatsby-browser.js'),
-      options: {"plugins":[],"name":"gatsby-starter-default","short_name":"starter","start_url":"/","background_color":"#663399","theme_color":"#663399","display":"minimal-ui","icon":"src/images/favicon.png","cache_busting_mode":"query","include_favicon":true,"legacy":true,"theme_color_in_head":true,"cacheDigest":"99515bf5f3b024aeb0dedf2727f2faa6"},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
