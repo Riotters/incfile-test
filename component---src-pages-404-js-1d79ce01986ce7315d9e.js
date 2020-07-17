393b583eb2442c865d20a506697ab837a9deb594
Krzysiek <krzysiekstefanski@gmail.com>
2020-07-17 16:10:37 +0200

    Updates

--- component---src-pages-404-js-1d79ce01986ce7315d9e.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),r=n("Bl7J"),c=n("vrFN"),o=n("+e4l");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"404: Not found"}),l.a.createElement("div",{style:{paddingTop:"120px",paddingBottom:"20px"}},l.a.createElement(o.a,null,l.a.createElement("h1",null,"404"),l.a.createElement("p",null,"NOT FOUND"))))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-1d79ce01986ce7315d9e.js.map
