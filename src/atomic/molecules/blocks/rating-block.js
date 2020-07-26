2613811da646ee3bbbc63b1f00532ccf13559821
Dennis Gabriel Kruk <dennisgabrielkruk@gmail.com>
2020-07-26 18:39:53 +0200

    Create rating-block.js

---
+++ src/atomic/molecules/blocks/rating-block.js
@@ -0,0 +1,19 @@
+import React from "react";
+import RatingBox from "../../atoms/boxes/cart-box";
+import Rating from "../../atoms/icons/rating-icon";
+import RatingDescription from "../../atoms/boxes/rating-description";
+import BInline from "../../../images/bbb-logo.inline.svg";
+
+const RatingBlock = () => (
+    <RatingBox>
+        <Rating>
+            <BInline />
+        </Rating>
+        <RatingDescription>
+            <span style={{fontWeight: "bold", color: "black", display: "block"}}>A+</span>
+            Rating
+        </RatingDescription>
+    </RatingBox>
+);
+
+export default RatingBlock;
