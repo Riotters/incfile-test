99bbd6606d6e85c858218d77ac4500d045b775bb
Dennis Gabriel Kruk <dennisgabrielkruk@gmail.com>
2020-07-28 18:17:00 +0200

    Update container.js

--- src/atomic/container.js
+++ src/atomic/container.js
@@ -1,5 +1,6 @@
 import React from "react"
 import styled from "styled-components"
+import PropTypes from "prop-types"
 
 const Wrapper = styled.div`
   position: relative;
@@ -9,11 +10,19 @@ const Wrapper = styled.div`
   padding-right: 15px;
   margin: 0 auto;
 
-  @media (min-width: 1230px) {
-    max-width: 1200px;
+  @media (min-width: ${props => props.width}px) {
+    max-width: ${props => props.width}px;
   }
 `
 
-const Container = ({ children }) => <Wrapper>{children}</Wrapper>
+const Container = ({ children, style, width}) => <Wrapper style={style} width={width}>{children}</Wrapper>
 
 export default Container
+
+Container.propTypes = {
+    width: PropTypes.number
+}
+
+Container.defaultProps = {
+    width: 1200
+}
