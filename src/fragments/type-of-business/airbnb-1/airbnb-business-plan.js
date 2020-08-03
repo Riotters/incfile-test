dd7f6942ebffb011bd89d7cc479b9713b7c419d7
Dennis Gabriel Kruk <dennisgabrielkruk@gmail.com>
2020-08-03 12:16:46 +0200

    Update airbnb-business-plan.js

--- src/fragments/type-of-business/airbnb-1/airbnb-business-plan.js
+++ src/fragments/type-of-business/airbnb-1/airbnb-business-plan.js
@@ -50,94 +50,99 @@ const AirbnbBusinessPlan = () => (
             <OvalGreenSVG/>
         </Oval>
         <OvalCenter>
-            <OvalSVG />
+            <OvalSVG/>
         </OvalCenter>
         <Container>
             <BusinessOwnerResponsibilities>
                 <Heading size={4} className="title">
                     Your Airbnb Business Plan
                 </Heading>
-                <Paragraph>
-                    You might not think you need a business plan for your Airbnb business—after all, if you’re getting
+                <Paragraph big>
+                    You might not think you need a business plan for your Airbnb business—after all, if you’re
+                    getting
                     guests
                     through Airbnb, and they’re paying, you don’t need a business plan, right? Wrong.
                 </Paragraph>
                 <Colorbox className="box" theme="icon" Icon={DominoIcon} color={color.yellow3} curve
                           curveColor={color.yellow1}>
                     <Heading size={4}>Why You Need a Business Plan?</Heading>
-                    <Paragraph>
-                        You need to define how you’re going to run your Airbnb business, market your property, get
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
+                        You need to define how you’re going to run your Airbnb business, market your property,
+                        get
                         sales,
                         make a
-                        profit and grow. This can only come about through creating a watertight business plan. It will
+                        profit and grow. This can only come about through creating a watertight business plan.
+                        It will
                         help
                         you
                         get your thinking in order and show your commitment to your Airbnb business.
                     </Paragraph>
                 </Colorbox>
 
-                <Paragraph>
-                    You will also need to look at financial projections for your Airbnb business. What are your expected
+                <Paragraph big>
+                    You will also need to look at financial projections for your Airbnb business. What are your
+                    expected
                     booking
-                    revenues? How much is it costing you to buy and maintain the property? What is your profitability?
+                    revenues? How much is it costing you to buy and maintain the property? What is your
+                    profitability?
                     How
                     much
                     money will you keep in the business to grow it? How much will you pay yourself and others?
                 </Paragraph>
-                <Paragraph style={{paddingBottom: "50px"}}>
+                <Paragraph big bottomPadding={50} bottomMargin={0}>
                     Business plans do vary slightly, but they should all cover the following areas:
                 </Paragraph>
 
                 <CountingTextBoxed number={1}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         An executive summary with the most important points from your business plan
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={2}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         Your goals and what you hope to achieve with your bar or club business
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={3}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         A description of your business, background information and context
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={4}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         A market analysis and likely demand
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={5}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         An overview of how your business is structured
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={6}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         Your business model
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={7}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         How you will market and sell your offerings
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={8}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         Financial projections, revenue and profitability
                     </Paragraph>
                 </CountingTextBoxed>
 
                 <CountingTextBoxed number={9}>
-                    <Paragraph bottomSpace={0}>
+                    <Paragraph bottomPadding={0} bottomMargin={0}>
                         Appendices
                     </Paragraph>
                 </CountingTextBoxed>
