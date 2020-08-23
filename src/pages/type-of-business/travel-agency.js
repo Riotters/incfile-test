49996116700a5a78ac50eb3c3741375a0a94f90c
Dennis Gabriel Kruk <lisekoliwer@gmail.com>
2020-08-23 21:01:12 +0200

    Create travel-agency.js

---
+++ src/pages/type-of-business/travel-agency.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Layout from "../../components/layout";
+import SEO from "../../components/seo";
+import Top from "../../components/partials/sections/top";
+import Button from "../../components/button";
+import RatingRow from "../../atomic/atoms/boxes/rating-row";
+import CartBlock from "../../atomic/molecules/blocks/cart-block";
+import RatingBlock from "../../atomic/molecules/blocks/rating-block";
+
+const TravelAgency = () => (
+    <Layout>
+        <SEO title="How to Start a Travel Agency"
+             description="Everyone likes traveling — new horizons, beautiful destinations and unique experiences mean we love packing our bags and spending time away from work and home."/>
+
+        <Top ovalColor="green" imageName="social-media-influencer" imageAlt="Mr Bulb with seedling" headlineWidth={700}>
+            <h1>How to Start a Travel Agency</h1>
+            <p>Everyone likes traveling — new horizons, beautiful
+                destinations and unique experiences mean we love packing
+                our bags and spending time away from work and home.</p>
+            <ButtonsBox>
+                <Button theme="primary56" marginMD="0 24px 0 0" arrow>
+                    Start Now
+                </Button>
+            </ButtonsBox>
+
+            <RatingRow>
+                <CartBlock/>
+                <RatingBlock/>
+            </RatingRow>
+        </Top>
+    </Layout>
+);
+
+export default TravelAgency;
