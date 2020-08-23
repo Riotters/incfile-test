4083629cf7d8f14d727c6d45ecd9111af9c2d13a
Dennis Gabriel Kruk <lisekoliwer@gmail.com>
2020-08-23 21:02:35 +0200

    Update travel-agency.js

--- src/pages/type-of-business/travel-agency.js
+++ src/pages/type-of-business/travel-agency.js
@@ -6,13 +6,14 @@ import Button from "../../components/button";
 import RatingRow from "../../atomic/atoms/boxes/rating-row";
 import CartBlock from "../../atomic/molecules/blocks/cart-block";
 import RatingBlock from "../../atomic/molecules/blocks/rating-block";
+import styled from "styled-components";
 
 const TravelAgency = () => (
     <Layout>
         <SEO title="How to Start a Travel Agency"
              description="Everyone likes traveling — new horizons, beautiful destinations and unique experiences mean we love packing our bags and spending time away from work and home."/>
 
-        <Top ovalColor="green" imageName="social-media-influencer" imageAlt="Mr Bulb with seedling" headlineWidth={700}>
+        <Top ovalColor="green" imageName="travel-agency" imageAlt="Mr Bulb with seedling" headlineWidth={700}>
             <h1>How to Start a Travel Agency</h1>
             <p>Everyone likes traveling — new horizons, beautiful
                 destinations and unique experiences mean we love packing
@@ -30,5 +31,14 @@ const TravelAgency = () => (
         </Top>
     </Layout>
 );
+
+const ButtonsBox = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media (min-width: 769px) {
+    flex-direction: row;
+  }
+`;
 
 export default TravelAgency;
