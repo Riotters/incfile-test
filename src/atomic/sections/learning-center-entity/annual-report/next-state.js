5f2edfe58bc31f73817ea4baffc3836cdc9cb979
Krzysiek <krzysiekstefanski@gmail.com>
2020-08-25 10:56:38 +0200

    in progress

--- src/atomic/sections/learning-center-entity/annual-report/next-state.js
+++ src/atomic/sections/learning-center-entity/annual-report/next-state.js
@@ -3,21 +3,14 @@ import styled from "styled-components";
 import { color } from "../../../atoms/styles/colors";
 import { Heading } from "../../../atoms/typography/heading";
 import { Paragraph } from "../../../atoms/typography/paragraph";
-import Buttonsbox from "../../../atoms/boxes/top-buttons-box";
-import Button from "../../../molecules/buttons/button";
-import ArrowLink from "../../../molecules/buttons/text";
 import ContentObject from "../../../partials/left-content-right-object";
-import IconSVG from "../../../../images/icons/biennal-report.inline.svg";
-import IconTextColorBox from "../../../molecules/text-blocks/icon-h4-text-color";
-import IconListColorBox from "../../../molecules/text-blocks/icon-h4-list-color";
-import NumericList from "../../../organisms/lists/numeric-boxed-list";
 import Oval from "../../../atoms/icons/oval";
-import OvalSVG from "../../../../images/ovals/top-left-transparent-blue3.inline.svg";
+import OvalSVG from "../../../../images/ovals/top-right-orange1-to-transparent.inline.svg";
+import Oval2SVG from "../../../../images/ovals/bottom-left-transparent-orange.inline.svg";
 
 const NextState = styled.section`
   position: relative;
-  padding-top: 64px;
-  padding-bottom: 120px;
+  padding: 108px 0;
   background-color: ${color.orange3};
 
   p {
@@ -31,10 +24,13 @@ const NextState = styled.section`
 
 const NextStateSection = ({ className, content }) => (
     <NextState className={className}>
-        <Oval className="oval" height="570" width="570" y="-40">
+        <Oval className="oval" height="720" width="720" top="0" right="0">
             <OvalSVG />
         </Oval>
-        <ContentObject object="">
+        <Oval className="oval" height="420" width="420" bottom="0" left="0">
+            <Oval2SVG />
+        </Oval>
+        <ContentObject object="" contentWidth="500">
             <Heading size="2" bottomMargin="48">{content.header}</Heading>
             <Heading size="4">{content.header2}</Heading>
             <Paragraph big bottomMargin="32">{content.text}</Paragraph>
