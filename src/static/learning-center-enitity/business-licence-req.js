cc1e1e19452f05b7bf09915f88d485242b0969bc
Riotolga <olga@riotters.com>
2020-08-26 11:15:55 +0200

    Create business-licence-req.js

---
+++ src/static/learning-center-enitity/business-licence-req.js
@@ -0,0 +1,38 @@
+export const top= {
+    header:
+    `Do I need a Business License?`,
+    text:
+    `Find out what licenses and permits you may need for 
+    your new business.`,
+} 
+export const about= {
+    header:
+    `Find out what licenses and permits you may need for your new business.`,
+    text:
+    `Whether you’re starting a home-based business or running a startup with multiple employees, it’s important to ensure you’re meeting all legal requirements at the federal, state, and local levels. The business licenses you need can depend on a variety of factors, including the services you offer, your location, the type of business you’re running, or the products you sell.`,
+}
+export const searchTool= {
+    header: 
+    `Incfile’s Business License Search Tool`,
+    text:
+    `Our Business License Search Tool helps you narrow down the requirements for your business. Just enter the relevant information below to learn the likely registrations, permits, or licenses you’ll need.`,
+    table: [
+        { header: 
+        `Your State`},
+        {
+            header:
+            `Your Industy`
+        },
+    ]
+    buttons:
+    [
+        {
+            header:
+            `Check Requirements`
+        }
+        {
+            url:
+            `/`
+        }
+    ]
+}
