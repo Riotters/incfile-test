e051ba6ab744780e69415b44e1d0be3f58bb0948
Riotolga <olga@riotters.com>
2020-08-26 16:01:24 +0200

    Create guide-to-start.js

---
+++ src/static/learning-center-enitity/guide-to-start.js
@@ -0,0 +1,38 @@
+export const top = {
+    header:
+    `So You Want to Start a Business?`,
+    text:
+    `We’ve Got You Covered—Everything You Need to Know to Launch Your New Venture`,
+    button: {
+        text:
+        `Download the complete guide`,
+        url:
+        `Download the complete guide`,
+    },
+}
+export const about = {
+    header:
+    `Starting a Business 101`,
+    text:
+    `Starting a new business is one of the most powerful and exciting things you can do. You’ve got a great idea, unique products and valuable services that you want to share with the world. There’s just one small problem — where do you begin? There seems to be an almost endless list of tasks to get your business off the ground and money in the bank.`,
+    card: [
+        {
+        header:
+        `Relax, we’ve got you covered.`,
+    },
+    {
+        text:
+        `We’re experts at incorporating LLCs and corporations, and we’ve learned a thing or two about starting and running businesses. That’s why we put together this complete and easy-to-understand business checklist — to make it faster and simpler for you to get stuff done.`,
+    }
+    ],
+    text:
+    `Whether you’re a new entrepreneur or a business veteran, use our start a business checklist to bring your ideas to life.`,
+    link: {
+        text:
+        `View our specialized guidelines`,
+        url:
+        `/`,
+    },
+
+    
+}
