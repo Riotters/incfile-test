a92ccac2f4b66af7ff7eeb177d3ba619ea1a0cef
Riotolga <olga@riotters.com>
2020-08-26 14:13:34 +0200

    Create start-a-business.js

---
+++ src/static/learning-center-enitity/start-a-business.js
@@ -0,0 +1,359 @@
+export const top = {
+    header:
+    `Start a New Business Today`,
+    text:
+    `Incfile has you covered with lots of business ideas and in-depth guides for success in different industries`,
+    button: {
+        text:
+        `Check out business types`,
+        url:
+        `/`,
+    },
+}
+export const about = {
+    header:
+    `Before you start a business`,
+    text:
+    `Before you start a new business, you want to make sure this new endeavor is going to be a right fit for you and for the market. `,
+    card: [
+        {header:
+            `Before you start a new business, you want to make sure this new endeavor is going to be a right fit for you and for the market. `,
+        },
+        {
+            text:
+            `Use our in-depth "Start a Business" guides to research everything you need to do before your doors open. `,
+        },
+    ],
+    text:
+    `Use our in-depth "Start a Business" guides to research everything you need to do before your doors open. `,
+    
+}
+export const checkType = {
+    header:
+    `Check out these types of businesses you could start`,
+    tabs: [
+            `Types`,
+            `Ideas`,
+    ],
+    buttons: [
+        {
+            header:
+            `Amazon Seller`,
+            url:
+            `/`
+        },
+        {
+            header:
+            `Franchise`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Home`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Teen`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Blockchain`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Digital Nomad`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `No Money`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Side`,
+            url:
+            `/`
+        },
+        {
+            header:
+            `Summer`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Social Media Influencer`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Etsy`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Online`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Women`,
+            url:
+            `/`,
+        },
+        {
+            header:
+            `Unique`,
+            url:
+            `/`
+        },
+    ],
+
+}
+export const businessStructure = {
+    header:
+    `Unsure which entity structure is best for your business?`,
+    cards: [
+        {
+            header:
+            `LLC`,
+            text:
+            `Keep personal assets separate from the company. `,
+        },
+        {
+            header:
+            `S – Corpotation`,
+            text:
+            `Keep personal assets separate from the company.`,
+        },
+        {
+            header:
+            `C – Corporation`,
+            text:
+            `Attractive to investors.`,
+        },
+        {
+            header:
+            `Non-Profit`,
+            text:
+            `Donations can be recordedby donors as not taxable. `
+        },
+
+    ],
+    button:
+    {
+        text:
+        `Compare entity types`,
+        url:
+        `/`,
+    },
+}
+export const launchBusiness = {
+    header:
+    `Launch your business with Incfile`,
+    text:
+    `No Contracts. No Surprise. Only $0 + State Fee to Launch Your Business.`,
+    button: 
+    {
+        text:
+        `launch your business`,
+        url:
+        `/`
+    },
+}
+export const checkIdeas = {
+    tabs: [
+        `Types`
+        `Ideas`
+    ],
+buttons: [
+    {
+        header:
+        `Bar or Club`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `T-shirt Printing`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Landscaping`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Real Estate`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Brewery`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Publishing`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Event Planner`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `B&B`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Moving Company`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Bakery Business`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Beauty Salon`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Cleaning`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Landromat`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Restaurant`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Daycare`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Travel Agency`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Bookstore`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Handyman`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Life Coach`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Record Label`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Catering`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Consulting`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Photography`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Dog Walking`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Construction`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Gym`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Food Truck`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Botique Clothing`,
+        url:
+        `/`,
+    },
+    {
+        header:
+        `Airbnb Business`,
+        url:
+        `/`,
+    },
+    
+]    
+} 
