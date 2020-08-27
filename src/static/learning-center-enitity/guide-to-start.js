160d505411f13b926fc9154fc14e98ab12ecc0a2
Riotolga <olga@riotters.com>
2020-08-27 11:20:38 +0200

    Update guide-to-start.js

--- src/static/learning-center-enitity/guide-to-start.js
+++ src/static/learning-center-enitity/guide-to-start.js
@@ -15,7 +15,7 @@ export const about = {
     `Starting a Business 101`,
     text:
     `Starting a new business is one of the most powerful and exciting things you can do. You’ve got a great idea, unique products and valuable services that you want to share with the world. There’s just one small problem — where do you begin? There seems to be an almost endless list of tasks to get your business off the ground and money in the bank.`,
-    card: [
+    box: [
         {
         header:
         `Relax, we’ve got you covered.`,
@@ -32,7 +32,118 @@ export const about = {
         `View our specialized guidelines`,
         url:
         `/`,
+    },   
+}
+export const tableOfContent = {
+    header:
+    `What You’ll Find in the Guide`,
+    title:
+    `Here’s a brief taster of what we cover:`,
+    cards: [
+        {
+            header:
+            `How Ready Are You to Start a Business?`,
+            text:
+            `Understanding your current skills and expertise, how to find out if your family is ready, deciding on the lifestyle you want and balancing running a business against having a day job.`,
+            link: 
+            {
+                text:
+                `Learn more`,
+                url:
+                `/`,
+            },
+        },
+        {
+            header:
+            `Developing Your Business Idea`,
+            text:
+            `Creating a business idea, doing your research and a guide to getting actionable, high-quality feedback.`,
+            link: 
+                {
+                    text:
+                    `Learn more`,
+                    url:
+                    `/`,
+                },
+            
+        },
+        {
+            header:
+            `Business Planning and Funding`,
+            title:
+            `Writing your business plan and what you should include in the document, business funding including bootstrapping, crowdfunding, loans, grants and lines of credit.`,
+            link:
+            {
+                text:
+                `Business Planning and Funding`,
+                url:
+                `/`,
+            },    
+        },
+        {
+            header:
+            `The Legalities of Starting a Business`,
+            text:
+            `Click here if you have existing registered agent service and would like to renew it.`,
+            link:
+            {
+                text:
+                `Learn more`,
+                url:
+                `/`,
+            },
+        },
+        {
+            header:
+            `Business Taxes, Licenses and Forms`,
+            text:
+            `Filing your business taxes, getting the right permits and licenses in place for your business and ongoing filing requirements so you stay compliant.`,
+            link:
+            {
+                text:
+                `Learn more`,
+                url:
+                `/`,
+            },
+        },
+        {
+            header:
+            `Business Identity and Marketing`,
+            text:
+            `Branding your business including your vision, mission statement, logos, marketing assets, physical and digital marketing channels and your small business website.`,
+            link:
+            {
+                text:
+                `Learn more`,
+                url:
+                `/`,
+            },
+        },
+        {
+            header:
+            `Learn more`,
+            text:
+            `Getting the right measurements in place to understand your return on investment, working with an accountant and understanding important financial reports.`,
+            link:
+            {
+                text:
+                `Learn more`,
+                url:
+                `/`,
+            },
+        },
+    ],
+}
+export const needMore = {
+    title:
+    `Need more information?`,
+    text:
+    `If that’s not enough, the guide also links to plenty of other in-depth resources to give you the expertise and confidence you need to start your business today. The time to start is now, download our guide to starting your business and start turning your vision into reality.`,
+    link:
+    {
+        text:
+        `Download the complete guide to starting a business now`,
+        url:
+        '/',
     },
-
-    
 }
