9c1513b6c491094419811079632848e1326acbb4
Riotolga <olga@riotters.com>
2020-09-02 13:09:00 +0200

    Update sales-tax-calculator.js

--- src/static/learning-center-entity/sales-tax-calculator.js
+++ src/static/learning-center-entity/sales-tax-calculator.js
@@ -2,6 +2,12 @@ export const top = {
   header: `Sales Tax Calculator`,
   text: `$0 + State Fee & FREE Registered Agent.`,
 };
+export const about = {
+  header:
+  `Quickly calclulate your sales taxes`,
+  text:
+  `Need to know what sales tax to charge your customers, or how much state sales tax you should be paying? Our easy-to-use calculator helps you look up the state sales tax rate for your area. Just enter your location below.`,
+};
 
 export const calculator = {
   header: `Calculate your tax amount`,
@@ -11,3 +17,10 @@ export const calculator = {
     url: `/`,
   },
 };
+export const additionaltax = {
+  header:
+  `Additional tax rates for a few of the top cities in California`,
+  text:
+  `Note: You may also need to add municipality and/or city rates depending on your location.`,
+};
+
