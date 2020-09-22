31da3fcb47ab958b8b5192460b5e665c248359ac
Krzysiek <krzysiekstefanski@gmail.com>
2020-09-22 11:16:59 +0200

    in progress

--- src/atomic/sections/other/how-it-works/steps.js
+++ src/atomic/sections/other/how-it-works/steps.js
@@ -6,6 +6,7 @@ import { Paragraph } from "../../../atoms/typography/paragraph";
 import HeadingCenter from "../../../partials/heading-center";
 import ContentCenter from "../../../partials/content-center";
 import Circle from "../../../atoms/icons/circle";
+import VariantsCards from "../../../organisms/cards/variants-cards.js";
 import List from "../../../organisms/lists/related-questions-list";
 import Oval from "../../../atoms/icons/oval";
 import Curve from "../../../atoms/icons/curve";
@@ -34,26 +35,41 @@ const Box = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-bottom: ${props => props.bottomMargin ? `${props.bottomMargin}px` : ""};
 
   span {
     font-size: 24px;
     line-height: 1;
+  }
+
+  h3, p {
+    text-align: center;
+  }
+
+  p {
+    max-width: 670px;
   }
 `;
 
 const StepsSection = ({ className, content }) => (
   <Steps className={className}>
     <HeadingCenter headline={content.header} text={content.text} headlineWidth="700" bottomMargin="80" />
-    <ContentCenter contentWidth="770">
-      <Box>
+    <ContentCenter>
+      <Box bottomMargin="120">
         <Circle height="64" width="64" circleColor={color.blue1} iconColor={color.white} bottomMargin="32">1</Circle>
         <Heading size="3" bottomMargin="24">{content.list[0].header}</Heading>
-        <Paragraph big>{content.list[0].text}</Paragraph>
+        <Paragraph big bottomMargin="0">{content.list[0].text}</Paragraph>
       </Box>
-      <Box>
+      <Box bottomMargin="80">
         <Circle height="64" width="64" circleColor={color.blue1} iconColor={color.white} bottomMargin="32">2</Circle>
         <Heading size="3" bottomMargin="24">{content.list[1].header}</Heading>
-        <Paragraph big>{content.list[1].text}</Paragraph>
+        <Paragraph big bottomMargin="0">{content.list[1].text}</Paragraph>
+      </Box>
+      <VariantsCards content={content.cards} bottomMargin="120" />
+      <Box>
+        <Circle height="64" width="64" circleColor={color.blue1} iconColor={color.white} bottomMargin="32">3</Circle>
+        <Heading size="3" bottomMargin="24">{content.list[2].header}</Heading>
+        <Paragraph big bottomMargin="0">{content.list[2].text}</Paragraph>
       </Box>
     </ContentCenter>
   </Steps>
