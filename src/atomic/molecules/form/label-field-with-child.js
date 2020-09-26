e1bf22a59c760307b59f33e8b8a00d258a748014
Dennis Gabriel Kruk <lisekoliwer@gmail.com>
2020-09-26 18:04:07 +0200

    Update forms

--- src/atomic/molecules/form/label-field-with-child.js
+++ src/atomic/molecules/form/label-field-with-child.js
@@ -24,10 +24,13 @@ const Wrapper = styled.div`
 
 const InputField = ({ className, children, content, htmlFor, bottomMargin, contentMarginTop}) => (
   <Wrapper className={className} bottomMargin={bottomMargin} contentMarginTop={contentMarginTop}>
-    <div className="top">
-      {content.label && <Label text={content.label} htmlFor={htmlFor} />}
-      {content.link && <Link linkUrl={content.link.url} linkText={content.link.text} bottomMargin="0" />}
-    </div>
+      {content &&
+          <div className="top">
+              {content.label && <Label text={content.label} htmlFor={htmlFor} />}
+              {content.link && <Link linkUrl={content.link.url} linkText={content.link.text} bottomMargin="0" />}
+          </div>
+      }
+
     <div className="bottom">{children}</div>
   </Wrapper>
 );
