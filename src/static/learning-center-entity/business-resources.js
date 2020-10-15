00dd988be5d673968c18a717dd258b6d584423a9
Krzysiek <krzysiekstefanski@gmail.com>
2020-10-15 12:18:18 +0200

    business resources page

--- src/static/learning-center-entity/business-resources.js
+++ src/static/learning-center-entity/business-resources.js
@@ -27,37 +27,13 @@ export const about = {
 export const resources = {
     header: `General Tools and Resources for Creating Your Business`,
     tabs: {
-        switchers: [`Start a business soon`, `Form a Business Today`],
+        switchers: [`Start a business soon`, `Form a Business Today`, `Grow a Business`],
         panels: [
             {
                 cards: [
                     {
                         header: `The Complete “Start Your Business Checklist”`,
                         text: `A clear and comprehensive guide to starting your business the right way.`,
-                        button: {
-                            text: `Download PDF`,
-                            url: `/`,
-                        },
-                    },
-                    {
-                        header: `Different Types of Corporate Structures`,
-                        text: `Learn about the four main types of legal business entities—LLCs, S Corporations, C Corporations and nonprofits.`,
-                        button: {
-                            text: `Download PDF`,
-                            url: `/`,
-                        },
-                    },
-                    {
-                        header: `Why Form a Limited Liability Company?`,
-                        text: `Reasons to form one of the simplest business structures: The Limited Liability Company (LLC).`,
-                        button: {
-                            text: `Download PDF`,
-                            url: `/`,
-                        },
-                    },
-                    {
-                        header: `What to Do After Forming Your LLC`,
-                        text: `Once you’ve incorporated your LLC, here’s what you need to do to maximize your chances of success.`,
                         button: {
                             text: `Download PDF`,
                             url: `/`,
@@ -88,8 +64,48 @@ export const resources = {
                         },
                     },
                     {
-                        header: `Different Types of Corporate Structures`,
-                        text: `Learn about the four main types of legal business entities—LLCs, S Corporations, C Corporations and nonprofits.`,
+                        header: `If You’re Not a U.S. Citizen, Can You Get an EIN for Your Business?`,
+                        text: `Non-US Citizens can still form a business.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Being Self-Employed is Awesome`,
+                        text: `Ready to start your own business? Check out this infographic on why it's awesome.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Search for and Choose the Perfect Business Name`,
+                        text: `Don't get stuck on your business name. Here are some resources to find the perfect one.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Creating the Right Business Name Helps Your Business Stand Out`,
+                        text: `Stand out from the crowd with a unique and memorable business name.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Launching a Food Truck Business`,
+                        text: `Take your passion for food to the streets!`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `How to Decide What Business Structure is Right for You`,
+                        text: `Choose a Business Entity Based on Specific Scenarios`,
                         button: {
                             text: `Download PDF`,
                             url: `/`,
@@ -100,8 +116,132 @@ export const resources = {
             {
                 cards: [
                     {
-                        header: `aaa`,
-                        text: `aaa`,
+                        header: `Different Types of Corporate Structures`,
+                        text: `Learn about the four main types of legal business entities—LLCs, S Corporations, C Corporations and nonprofits.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `How to Find a Registered Agent`,
+                        text: `A resource to understand what registered agents do and the Incfile registered agent service.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `What to Do After Forming Your LLC`,
+                        text: `Once you’ve incorporated your LLC, here’s what you need to do to maximize your chances of success.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Why Form a Limited Liability Company?`,
+                        text: `Reasons to form one of the simplest business structures: The Limited Liability Company (LLC).`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Why Form an S Corporation?`,
+                        text: `Why you might choose to create an S Corporation and the areas you need to think about.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Why Form a C Corporation?`,
+                        text: `The advantages and disadvantages of creating a C Corporation.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `How to Start an LLC in California`,
+                        text: `A complete business resource for creating your business in The Golden State.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `How to Start an LLC in Florida`,
+                        text: `Learn how to create a new business in The Sunshine State.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `How to Create and File an LLC for Free`,
+                        text: `Want to know how to create and file an LLC for free? Download our guide to see how!`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `What is the Best State to Start My LLC?`,
+                        text: `All states have pros & cons for forming LLCs. Find out which state is best for you.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Great Reasons to Start an LLC in Nevada`,
+                        text: `Nevada is more than just gaming and the Vegas strip. Find out why it might be the best place for your LLC.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `LLC Operating Agreements`,
+                        text: `Understand the ins and outs of LLC Operating Agreements.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                ],
+            },
+            {
+                cards: [
+                    {
+                        header: `Do LLCs Get a 1099 During Tax Time?`,
+                        text: `Tax time can be confusing. Make sure if you're supposed to get a 1099, you do.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `The Guide To Moving Your LLC to Another State`,
+                        text: `If you're moving, make sure you don't leave your LLC behind.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Can an LLC Owner File for Unemployment?`,
+                        text: `During trying times, unemployment might be a good option for LLC owners.`,
+                        button: {
+                            text: `Download PDF`,
+                            url: `/`,
+                        },
+                    },
+                    {
+                        header: `Paying Yourself as an LLC Owner`,
+                        text: `How much and how often? Learn more about paying yourself as an LLC owner.`,
                         button: {
                             text: `Download PDF`,
                             url: `/`,
