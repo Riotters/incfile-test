7c9cf9d7d36ac6bd3810eb9fe795e6fca39ac36e
Krzysztof <krzysztof@riotterd.com>
2020-10-20 13:22:21 +0200

    Update alaska-llc.js

--- src/pages/alaska-llc.js
+++ src/pages/alaska-llc.js
@@ -5,12 +5,12 @@ import SEO from "../components/seo";
 // Components
 import LeftTabPages from "../atomic/states-llc/left-tab-pages";
 import MainPageContent from "../atomic/states-llc/page-content";
-import LLCLayout from "../atomic/partials/states-llc/louisiana";
+import LLCLayout from "../atomic/partials/states-llc/alaska";
 import ContentMap from "../atomic/partials/content-map";
 
 // Content
-import { howToGuide } from "../static/states-llc/louisiana/home";
-import { tabPages } from "../static/states-llc/louisiana/general";
+import { howToGuide } from "../static/states-llc/alaska/home";
+import { tabPages } from "../static/states-llc/alaska/general";
 
 function LLCIndex() {
   return (
@@ -19,7 +19,7 @@ function LLCIndex() {
       <LLCLayout>
         <LeftTabPages content={tabPages} nonDIY />
         <MainPageContent>
-            <ContentMap content={howToGuide} />
+          <ContentMap content={howToGuide} />
         </MainPageContent>
       </LLCLayout>
     </Layout>
