87aebf9ca3c6288a3bfafb072aa3a8b0e812b95f
Hung Nguyen <tinapc@gmail.com>
2020-10-25 11:37:36 -0500

    No message

--- src/pages/california-corporation/start-a-ca-corporation.js
+++ src/pages/california-corporation/start-a-ca-corporation.js
@@ -13,36 +13,46 @@ import Variants from "../../atomic/sections/learning-center-entity/state-informa
 import Subscription from "../../atomic/sections/learning-center-entity/state-information-california/subscription";
 import Articles from "../../atomic/sections/articles";
 //Texts
-import { top, tabPages, startCaCorp, launchBusiness, requirements, learnMore, quickLinks, variants, subscription } from "../../static/learning-center-entity/state-information-california";
+import { top, tabPages, startCaCorp, launchBusiness, subscription } from "../../static/learning-center-entity/state-information-california";
 import LeftTabPages from "../../atomic/sections/choose-your-business/left-tab-pages";
 import MainPageContent from "../../atomic/states-llc/page-content";
 import ContentMap from "../../atomic/partials/content-map";
 import CorporationLayout from "../../atomic/partials/corporation-layout";
+import { getFullPricesAndFilings } from '../../api/Api';
 
-const StateInformation = () => (
-    <Layout>
-        <SEO title="California Corporations & Other Business Entities " description="Thinking of starting an S Corp, C Corp, or LLC in California? Learn about corporations in CA and decide which business type is right for you." />
-        <Top imageName="mr-bulb-corporation-california-8302" imageAlt="Mrs Bulb and with checklist" ovalColor="purple-2" textWidth="530">
-            <h1>{top.header}</h1>
-            <p>{top.text}</p>
-            <Buttonsbox>
-                <Button content={top.buttons[0]} theme="primary56" arrow />
-            </Buttonsbox>
-        </Top>
-        <CorporationLayout>
-            <LeftTabPages content={tabPages} nonDIY />
-            <MainPageContent>
-                <ContentMap content={startCaCorp} />
-            </MainPageContent>
-        </CorporationLayout>
-        <Rocket content={launchBusiness} />
-        <Requirements content={requirements} />
-        <LearnMore content={learnMore} />
-        <QuickLinks content={quickLinks} />
-        <Variants content={variants} />
-        <Subscription content={subscription} />
-        <Articles />
-    </Layout>
-);
+const CaliforniaCorporation = () => {
+    const [dataApi, setDataApi] = React.useState({});
+    React.useEffect(() => {
+        getFullPricesAndFilings('California', 'Corporation').then(data => {
+            setDataApi(data);
+        });
+    }, []);
 
-export default StateInformation;
+    return (
+        <Layout>
+            <SEO title="California Corporations & Other Business Entities " description="Thinking of starting an S Corp, C Corp, or LLC in California? Learn about corporations in CA and decide which business type is right for you." />
+            <Top imageName="mr-bulb-corporation-california-8302" imageAlt="Mrs Bulb and with checklist" ovalColor="purple-2" textWidth="530">
+                <h1>{top.header}</h1>
+                <p>{top.text}</p>
+                <Buttonsbox>
+                    <Button content={top.buttons[0]} theme="primary56" arrow />
+                </Buttonsbox>
+            </Top>
+            <CorporationLayout>
+                <LeftTabPages content={tabPages} nonDIY />
+                <MainPageContent>
+                    <ContentMap content={startCaCorp} />
+                </MainPageContent>
+            </CorporationLayout>
+            <Rocket content={launchBusiness} />
+            <Requirements data={dataApi} />
+            <LearnMore data={dataApi} />
+            <QuickLinks />
+            <Variants data={dataApi} />
+            <Subscription content={subscription} />
+            <Articles />
+        </Layout>
+    );
+}
+
+export default CaliforniaCorporation;
--- src/pages/delaware-corporation/taxes-and-fees-for-your-corporation.js
+++ src/pages/delaware-corporation/taxes-and-fees-for-your-corporation.js
@@ -13,36 +13,46 @@ import Variants from "../../atomic/sections/learning-center-entity/state-informa
 import Subscription from "../../atomic/sections/learning-center-entity/state-information-california/subscription";
 import Articles from "../../atomic/sections/articles";
 //Texts
-import { top, tabPages, taxes, launchBusiness, requirements, learnMore, quickLinks, variants, subscription } from "../../static/learning-center-entity/state-information-delaware";
+import { top, tabPages, taxes, launchBusiness, subscription } from "../../static/learning-center-entity/state-information-delaware";
 import LeftTabPages from "../../atomic/sections/choose-your-business/left-tab-pages";
 import MainPageContent from "../../atomic/states-llc/page-content";
 import ContentMap from "../../atomic/partials/content-map";
 import CorporationLayout from "../../atomic/partials/corporation-layout";
 
-const StateInformation = () => (
-    <Layout>
-        <SEO title="Business Taxes & Fees for Delaware S Corps & C Corps" description="Learn about the taxes and fees you&#039;ll be required to pay for your Delaware corporation, plus find out the tax differences between DE S Corps and C Corps." />
-        <Top imageName="mr-bulb-corporation-california-8302" imageAlt="Mrs Bulb and with checklist" ovalColor="purple-2" textWidth="530">
-            <h1>{top.header}</h1>
-            <p>{top.text}</p>
-            <Buttonsbox>
-                <Button content={top.buttons[0]} theme="primary56" arrow />
-            </Buttonsbox>
-        </Top>
-        <CorporationLayout>
-            <LeftTabPages content={tabPages} nonDIY />
-            <MainPageContent>
-                <ContentMap content={taxes} />
-            </MainPageContent>
-        </CorporationLayout>
-        <Rocket content={launchBusiness} />
-        <Requirements content={requirements} />
-        <LearnMore content={learnMore} />
-        <QuickLinks content={quickLinks} />
-        <Variants content={variants} />
-        <Subscription content={subscription} />
-        <Articles />
-    </Layout>
-);
+import { getFullPricesAndFilings } from '../../api/Api';
 
-export default StateInformation;
+const DelawareCorporation = () => {
+    const [dataApi, setDataApi] = React.useState({});
+    React.useEffect(() => {
+        getFullPricesAndFilings('Delaware', 'Corporation').then(data => {
+            setDataApi(data);
+        });
+    }, []);
+    return (
+        <Layout>
+            <SEO title="Business Taxes & Fees for Delaware S Corps & C Corps" description="Learn about the taxes and fees you&#039;ll be required to pay for your Delaware corporation, plus find out the tax differences between DE S Corps and C Corps." />
+            <Top imageName="mr-bulb-corporation-california-8302" imageAlt="Mrs Bulb and with checklist" ovalColor="purple-2" textWidth="530">
+                <h1>{top.header}</h1>
+                <p>{top.text}</p>
+                <Buttonsbox>
+                    <Button content={top.buttons[0]} theme="primary56" arrow />
+                </Buttonsbox>
+            </Top>
+            <CorporationLayout>
+                <LeftTabPages content={tabPages} nonDIY />
+                <MainPageContent>
+                    <ContentMap content={taxes} />
+                </MainPageContent>
+            </CorporationLayout>
+            <Rocket content={launchBusiness} />
+            <Requirements data={dataApi} />
+            <LearnMore data={dataApi} />
+            <QuickLinks />
+            <Variants data={dataApi} />
+            <Subscription content={subscription} />
+            <Articles />
+        </Layout>
+    );
+}
+
+export default DelawareCorporation;
