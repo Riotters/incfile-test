86662adf98a64422b561388d06274f1b9b2c7c62
Hung Nguyen <tinapc@gmail.com>
2020-10-26 03:30:37 -0500

    hh

--- src/pages/compliance-filing-requirement.js
+++ src/pages/compliance-filing-requirement.js
@@ -1,29 +1,46 @@
 import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
-import Buttonsbox from "../atomic/atoms/boxes/top-buttons-box";
-import Button from "../atomic/molecules/buttons/button";
+
 //Sections
 import Top from "../atomic/partials/top";
 import About from "../atomic/sections/learning-center-entity/ongoing-filing-requirements/about";
 import EntityTypeAndState from "../atomic/sections/learning-center-entity/ongoing-filing-requirements/entity-type-and-state";
-import Requirements from "../atomic/sections/learning-center-entity/ongoing-filing-requirements/requirements";
+import TextCenterLayout from "../components/partials/blocks/heading-center";
 import Rocket from "../atomic/sections/review-entity-types/c-corporation/rocket";
-//Texts
-import { top, about, entityTypeAndState, requirements, rocket } from "../static/learning-center-entity/ongoing-filing-requirements";
+import FilingTimeAndPriceBox from '../atomic/state-corporation/filing-time-and-price-box';
+import ComplianceRequirementBox from "../atomic/state-corporation/compliance-requirement-box";
+import ContentCenter from "../atomic/partials/content-center";
 
-const OngoingFilingRequirements = () => (
-  <Layout>
-    <SEO title="Research State by State Compliance Filing Requirements" description="Research State by State Compliance Filing Requirements" />
-    <Top imageName="mrs-bulb-ongoing-filing-requirements-7487" imageAlt="Mrs Bulb and with checklist" ovalColor="green">
-      <h1>{top.header}</h1>
-      <p>{top.text}</p>
-    </Top>
-    <About content={about} />
-    <EntityTypeAndState content={entityTypeAndState} />
-    <Requirements content={requirements} />
-    <Rocket content={rocket} />
-  </Layout>
-);
+//Texts
+import { top, about, entityTypeAndState, rocket } from "../static/learning-center-entity/ongoing-filing-requirements";
+
+const OngoingFilingRequirements = () => {
+    const [dataApi, setDataApi] = React.useState('');
+    const [header, setHeader] = React.useState('');
+    const renderComplianceSection = (data) => {
+        setDataApi(data);
+        setHeader(`Mandatory ${data.prices.state} Compliance Requirements`);
+    }
+    return (
+        <Layout>
+            <SEO title="Research State by State Compliance Filing Requirements" description="Research State by State Compliance Filing Requirements" />
+            <Top imageName="mrs-bulb-ongoing-filing-requirements-7487" imageAlt="Mrs Bulb and with checklist" ovalColor="green">
+                <h1>{top.header}</h1>
+                <p>{top.text}</p>
+            </Top>
+            <About content={about} />
+            <EntityTypeAndState content={entityTypeAndState} getDataApi={renderComplianceSection} />
+            {dataApi.length && (
+                <ContentCenter contentWidth="970">
+                    <TextCenterLayout headline={header} headlineWidth="700" style={{ marginBottom: `52px` }} />
+                    <FilingTimeAndPriceBox data={dataApi} />
+                    <ComplianceRequirementBox data={dataApi} />
+                </ContentCenter>
+            )}
+            <Rocket content={rocket} />
+        </Layout>
+    );
+};
 
 export default OngoingFilingRequirements;
