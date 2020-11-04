217343b7c1e1fa130e3840ef0ce27474fe98536a
Hung Nguyen <tinapc@gmail.com>
2020-11-04 03:47:29 -0600

    no message

--- src/pages/georgia-entity-search.js
+++ src/pages/georgia-entity-search.js
@@ -2,7 +2,7 @@ import React from "react";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import Buttonsbox from "../atomic/atoms/boxes/top-buttons-box";
-import Button from "../atomic/molecules/buttons/button";
+import Button from "../atomic/molecules/buttons/button-action";
 //Sections
 import Top from "../atomic/partials/top";
 import About from "../atomic/sections/entity-search/georgia-entity-search/about";
@@ -11,33 +11,28 @@ import NewRules from "../atomic/sections/entity-search/georgia-entity-search/new
 import SpecificRules from "../atomic/sections/entity-search/georgia-entity-search/specific-rules";
 import StartBusiness from "../atomic/sections/entity-search/georgia-entity-search/start-business";
 import Faq from "../atomic/sections/entity-search/georgia-entity-search/faq";
+import {scrollToElement } from '../helpers/utils';
+
 //Texts
 import { top, about, search, newRules, specificRules, startBusiness, faq } from "../static/entity-search/georgia-entity-search";
 
-const GeneralResearchTopics = () => (
-  <Layout>
-    <SEO title="Georiga Entity Search" description="aaa" />
-    <Top imageName="mr-bulb-corporation-california-8302" imageAlt="Mrs Bulb and with checklist" ovalColor="love" headlineWidth="700" textWidth="400">
-      <h1>{top.header}</h1>
-      <p>{top.text}</p>
-      <Buttonsbox>
-        <Button content={top.button[0]} theme="primary56" arrow onClick={e => handleMenuStepClick("check-name", e)}/>
-      </Buttonsbox>
-    </Top>
-    <About content={about} />
-    <Search content={search} />
-    <NewRules content={newRules} />
-    <SpecificRules content={specificRules} />
-    <StartBusiness content={startBusiness} />
-    <Faq content={faq} />
-  </Layout>
+const GeorgiaEntitySearch = () => (
+    <Layout>
+        <SEO title="Georiga Entity Search" description="aaa" />
+        <Top imageName="mr-bulb-corporation-california-8302" imageAlt="Mrs Bulb and with checklist" ovalColor="love" headlineWidth="700" textWidth="400">
+            <h1>{top.header}</h1>
+            <p>{top.text}</p>
+            <Buttonsbox>
+                <Button onClick={e => scrollToElement(e, 'js-business-name-search-form')} content={top.button[0]} theme="primary56" arrow />
+            </Buttonsbox>
+        </Top>
+        <About content={about} />
+        <Search content={search} id="js-business-name-search-form" />
+        <NewRules content={newRules} />
+        <SpecificRules content={specificRules} />
+        <StartBusiness content={startBusiness} />
+        <Faq content={faq} />
+    </Layout>
 );
 
-export default GeneralResearchTopics;
-
-export function handleMenuStepClick(l, e) {
-  e.preventDefault();
-
-  const el = document.getElementById(l);
-  el.scrollIntoView({ behavior: 'smooth', block: 'start'});
-}
+export default GeorgiaEntitySearch;
