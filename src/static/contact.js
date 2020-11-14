09c096c5c42d2068b20d712985b006389ba40a75
Hung Nguyen <tinapc@gmail.com>
2020-11-14 13:51:40 -0600

    no message

--- src/static/contact.js
+++ src/static/contact.js
@@ -9,7 +9,7 @@ export const existing = {
   },
   phone: {
     phone1: `844.830.8267`,
-    phone2: `877 919-2613`,
+    phone2: `877.919.2613`,
   },
 };
 
@@ -17,7 +17,7 @@ export const newSales = {
   header: `We've put together the following resources to help answer our most common questions.`,
   phone: {
     phone1: `855.829.9090`,
-    phone2: `877 919-2613`,
+    phone2: `877.919.2613`,
   },
   articles: [
     {
