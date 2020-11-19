5d317a02961fda14ddaa1edd5a11e4a480478a72
omgxghxgf <a.brylanska@wp.pl>
2020-11-19 12:22:52 +0100

    regulations

--- src/atomic/sections/type-of-business/women-business/regulations.js
+++ src/atomic/sections/type-of-business/women-business/regulations.js
@@ -15,6 +15,15 @@ const Regulations = () => (
         Whether your business is woman-owned or not, almost no company can escape real-world licenses, permits and regulations. You will need to explore local, state and federal licenses and permits, and{" "}
         <Link to="/business-license-research-package/">we can also help out with researching your business licensing needs</Link>.
       </Paragraph>
+      <Heading size={4} style={{ fontSize: "24px" }}>
+        Finances
+      </Heading>
+      <Paragraph big mixed={true}>
+        In most cases, you will want to keep your personal and business finances separate. We recommend a <Link to="https://utility.bankofamerica.com/alliance/?formid=IF1" target="_blank" rel="noopener noreferrer">business bank account</Link> for your side business. You might also want to consider a <Link to="http://t.tihop.com/cgi/r?;n=203;c=1666272;s=7273;x=7936;f=201908011042310;u=j;z=TIMESTAMP;src=663024">business credit card</Link>. Additionally, you will also need to keep careful bookkeeping records and file business and personal taxes.
+      </Paragraph>
+      <Heading size={4} style={{ fontSize: "24px" }}>
+        Taxes
+      </Heading>
       <Paragraph big>
         Taxes are a fact of life if you’re in business, and there are various ways you will need to file and pay them. This can include self-employment tax, state income tax and federal income tax. Depending on where and how you’re selling products and services, you may also be liable for sales and
         use tax.
