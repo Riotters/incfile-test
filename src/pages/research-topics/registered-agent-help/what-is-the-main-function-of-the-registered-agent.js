e1e2da95a7d1e2a212e5d507bb657eae974dae05
Riotters <krzysztof@riotters.com>
2020-12-05 16:56:45 +0100

    helmet missing fix

--- src/pages/research-topics/registered-agent-help/what-is-the-main-function-of-the-registered-agent.js
+++ src/pages/research-topics/registered-agent-help/what-is-the-main-function-of-the-registered-agent.js
@@ -11,49 +11,61 @@ import Tools from "../../../atomic/sections/learning-center-entity/help-center-a
 import Rocket from "../../../atomic/sections/review-entity-types/c-corporation/rocket";
 import Searchbar from "../../../atomic/molecules/form/help-center-searchbar";
 //Texts
-import { top, rocket, categories, service, tools } from "../../../static/learning-center-entity/help-center-article";
+import {
+	top,
+	rocket,
+	categories,
+	service,
+	tools,
+} from "../../../static/learning-center-entity/help-center-article";
 import { about } from "../../../static/research-topics/registered-agent/registered-agent-function";
 import { related } from "../../../static/research-topics/registered-agent";
+import { Helmet } from "react-helmet";
 
 const HelpCenter = () => {
-    const currentUri = typeof window !== 'undefined' ? window.location.pathname : '';
-    const relatedInfo = {
-        header: `Related Articles`,
-        text: `Other users were also interested in knowing about…`,
-        buttons: related.buttons.filter(item => {
-            return item.url !== currentUri;
-        })
-    };
+	const currentUri =
+		typeof window !== "undefined" ? window.location.pathname : "";
+	const relatedInfo = {
+		header: `Related Articles`,
+		text: `Other users were also interested in knowing about…`,
+		buttons: related.buttons.filter((item) => {
+			return item.url !== currentUri;
+		}),
+	};
 
-    React.useEffect(() => {
-        const el = document.getElementById('answers');
-        el.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
-        });
-    })
+	React.useEffect(() => {
+		const el = document.getElementById("answers");
+		el.scrollIntoView({
+			behavior: "smooth",
+			block: "start",
+		});
+	});
 
-    return (
-        <Layout>
-            <SEO
-                title="What is the main function of the Registered Agent?"
-                description="Q & A for LLC and other forms of corporations. Learning Center for
+	return (
+		<Layout>
+			<SEO
+				title="What is the main function of the Registered Agent?"
+				description="Q & A for LLC and other forms of corporations. Learning Center for
     Incorporating in every state."
-            />{" "}
-            <Top imageName="mrs-bulb-help-center-article" imageAlt="Mrs Bulb and with checklist" ovalColor="green">
-                <h1>{top.header}</h1>
-                <p>{top.text}</p>
-                <Searchbar />
-            </Top>
-            <About content={about} />
-            <Related content={relatedInfo} />
-            <Categories content={categories} />
-            <Service content={service} />
-            <Tools content={tools} />
-            <Rocket content={rocket} />
-            <Helmet>
-                <script type="application/ld+json">
-                    {`{
+			/>{" "}
+			<Top
+				imageName="mrs-bulb-help-center-article"
+				imageAlt="Mrs Bulb and with checklist"
+				ovalColor="green"
+			>
+				<h1>{top.header}</h1>
+				<p>{top.text}</p>
+				<Searchbar />
+			</Top>
+			<About content={about} />
+			<Related content={relatedInfo} />
+			<Categories content={categories} />
+			<Service content={service} />
+			<Tools content={tools} />
+			<Rocket content={rocket} />
+			<Helmet>
+				<script type="application/ld+json">
+					{`{
                         "@context": "https://schema.org",
                         "@type": "FAQPage",
                         "mainEntity": {
@@ -68,10 +80,10 @@ const HelpCenter = () => {
                         governing tax authority."
                         }}
                     }`}
-                </script>
-            </Helmet>
-        </Layout>
-    );
-}
+				</script>
+			</Helmet>
+		</Layout>
+	);
+};
 
 export default HelpCenter;
