915850cb8f61f220a311de8f42fa095895cbb893
Riotters <krzysztof@riotters.com>
2020-12-05 18:54:43 +0100

    fixes

--- src/atomic/sections/other/sitemap/top.js
+++ src/atomic/sections/other/sitemap/top.js
@@ -4,30 +4,36 @@ import { color } from "../../../../components/styles/colors";
 import HeadingCenter from "../../../partials/heading-center";
 import Curve from "../../../atoms/icons/curve";
 import CurveSVG from "../../../../images/curves/bottom-left-top-right-big.inline.svg";
+import ContentCenter from "../../../partials/content-center";
+import { Heading } from "../../../atoms/typography/heading";
 
 const Wrapper = styled.div`
-  padding: 120px 0;
-  position: relative;
+	padding: 120px 0;
+	position: relative;
 
-  .oval {
-    top: 36%;
-    left: 36%;
-  }
+	.oval {
+		top: 36%;
+		left: 36%;
+	}
 
-  .heading {
-    padding-top: 136px;
-  }
+	h1 {
+		padding-top: 136px;
+	}
 `;
 
 function Top({ content }) {
-  return (
-    <Wrapper>
-      <Curve className="oval" rotate="-5" color={color.orange1}>
-        <CurveSVG />
-      </Curve>
-      <HeadingCenter className="heading" headline={content.header} headlineWidth="770" />
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<Curve className="oval" rotate="-5" color={color.orange1}>
+				<CurveSVG />
+			</Curve>
+			<ContentCenter>
+				<Heading size={1} template={2} maxWidth="770">
+					{content.header}
+				</Heading>
+			</ContentCenter>
+		</Wrapper>
+	);
 }
 
 export default Top;
--- src/static/learning-center-entity/ongoing-filing-requirements.js
+++ src/static/learning-center-entity/ongoing-filing-requirements.js
@@ -1,58 +1,58 @@
 export const top = {
-  header: `Research State Ongoing Filing Requirements`,
-  text: `Learn the requirements for ongoing filing requests in your state to remain in good standing.`,
+	header: `Research State Ongoing Filing Requirements.`,
+	text: `Learn the requirements for ongoing filing requests in your state to remain in good standing.`,
 };
 
 export const about = {
-  header: `State by State Review of Ongoing Filing Requirements`,
-  text: `Filing requirements are an important part in maintaining a companies standing with the state. Each state proscribes specific rules regarding when annual reports and franchise tax reports are due. Many states require the annual report to be filed on the anniversary month of the companies date of formation. Where some states set a specific date of filing for corporations and another for LLCs.`,
-  box: {
-    header: `The reports are mandatory`,
-    text: `…and must be filed within the specified time frame in order for the entity to remain in good standing with the state. Failure to file these reports can lead to the company being revoked or administrative dissolution.`,
-  },
-  text2: `Included with all incorporation packages are lifetime company alerts that will provide courtesy email reminders to inform clients of an upcoming filing requirement.`,
+	header: `State by State Review of Ongoing Filing Requirements`,
+	text: `Filing requirements are an important part in maintaining a companies standing with the state. Each state proscribes specific rules regarding when annual reports and franchise tax reports are due. Many states require the annual report to be filed on the anniversary month of the companies date of formation. Where some states set a specific date of filing for corporations and another for LLCs.`,
+	box: {
+		header: `The reports are mandatory`,
+		text: `…and must be filed within the specified time frame in order for the entity to remain in good standing with the state. Failure to file these reports can lead to the company being revoked or administrative dissolution.`,
+	},
+	text2: `Included with all incorporation packages are lifetime company alerts that will provide courtesy email reminders to inform clients of an upcoming filing requirement.`,
 };
 
 export const entityTypeAndState = {
-  header: `Choose your entity type and state`,
-  text:``,
-  cards: [`1. Entity Type`, `2. Your State`],
+	header: `Choose your entity type and state`,
+	text: ``,
+	cards: [`1. Entity Type`, `2. Your State`],
 };
 
 export const requirements = {
-  header: `Mandatory California Compliance Requirements`,
-  columns: [
-    {
-      header: `Filing time and price`,
-      text: `Frequency: Annually`,
-      text2: `Due Date: During six-month period ending on last day of anniversary month of incorporation or qualification.`,
-      link: {
-        text: `State Filing Times`,
-        url: `/state-filing-times/`,
-      },
-    },
-    {
-      header: `Compliance Requirements`,
-      text: `Filing Fee: $25`,
-      text2: `Important: The initial Statement of Information filing is due within 90 days of the entity formation date.`,
-      link: {
-        text: `State Filing Prices`,
-        url: `/state-filing-fees/`,
-      },
-    },
-  ],
-  header2: `California Franchise Tax`,
-  text: `$800 payment for the LLC Franchise Tax is due by 15th day of the 4th month after your LLC is filed. The month your LLC is filed counts as Month 1, regardless if you file on the 1st of the month, the last of the month, or any day of the month, really.`,
-  text2: `This means that if you were to file your LLC on March 22nd, then you must pay the $800 fee no later than June 15th (in this example, March is Month 1, April is Month 2, May is Month 3, and June is Month 4). Then, every year after your first payment $800 LLC Franchise Tax will be due April 15th. You pay the $800 LLC Franchise Tax using Form 3522 called the LLC Tax Voucher.`,
+	header: `Mandatory California Compliance Requirements`,
+	columns: [
+		{
+			header: `Filing time and price`,
+			text: `Frequency: Annually`,
+			text2: `Due Date: During six-month period ending on last day of anniversary month of incorporation or qualification.`,
+			link: {
+				text: `State Filing Times`,
+				url: `/state-filing-times/`,
+			},
+		},
+		{
+			header: `Compliance Requirements`,
+			text: `Filing Fee: $25`,
+			text2: `Important: The initial Statement of Information filing is due within 90 days of the entity formation date.`,
+			link: {
+				text: `State Filing Prices`,
+				url: `/state-filing-fees/`,
+			},
+		},
+	],
+	header2: `California Franchise Tax`,
+	text: `$800 payment for the LLC Franchise Tax is due by 15th day of the 4th month after your LLC is filed. The month your LLC is filed counts as Month 1, regardless if you file on the 1st of the month, the last of the month, or any day of the month, really.`,
+	text2: `This means that if you were to file your LLC on March 22nd, then you must pay the $800 fee no later than June 15th (in this example, March is Month 1, April is Month 2, May is Month 3, and June is Month 4). Then, every year after your first payment $800 LLC Franchise Tax will be due April 15th. You pay the $800 LLC Franchise Tax using Form 3522 called the LLC Tax Voucher.`,
 };
 
 export const rocket = {
-  header: `Launch`,
-  header2: `your business with Incfile`,
-  text: `No Contracts. No Surprises.`,
-  text2: `Only $0 + State Fee to Launch Your Business.`,
-  button: {
-    text: `Launch your business`,
-    url: `${process.env.ORDER_URL}/form-order-now.php`,
-  },
+	header: `Launch`,
+	header2: `your business with Incfile`,
+	text: `No Contracts. No Surprises.`,
+	text2: `Only $0 + State Fee to Launch Your Business.`,
+	button: {
+		text: `Launch your business`,
+		url: `${process.env.ORDER_URL}/form-order-now.php`,
+	},
 };
