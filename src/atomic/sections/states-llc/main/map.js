483c1a800a28b5aa7f6163203f5a3608511543bf
Riotters <krzysztof@riotters.com>
2020-12-05 22:26:22 +0100

    Revert "Revert "fixes""
    This reverts commit e46131503fa75adcab1c84c799af3129e038a116.

--- src/atomic/sections/states-llc/main/map.js
+++ src/atomic/sections/states-llc/main/map.js
@@ -6,33 +6,40 @@ import ContentCenter from "../../../partials/content-center";
 import StatesMap from "../../../../components/states-map/states-map";
 import Oval from "../../../atoms/icons/oval";
 import OvalSVG from "../../../../images/ovals/bottom-left-transparent-blue3.inline.svg";
+import { Heading } from "../../../atoms/typography/heading";
 
 const Map = styled.div`
-  padding-top: 180px;
-  padding-bottom: 40px;
-  position: relative;
+	padding-top: 180px;
+	padding-bottom: 40px;
+	position: relative;
 
-  &::before {
-    content: "";
-    height: 1564px;
-    width: 100%;
-    background-image: ${gradient.blue3};
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
+	&::before {
+		content: "";
+		height: 1564px;
+		width: 100%;
+		background-image: ${gradient.blue3};
+		position: absolute;
+		top: 0;
+		left: 0;
+	}
+
+	h1 {
+		text-align: center;
+	}
 `;
 
 const MapSection = ({ className, content }) => (
-  <Map className={className}>
-    <Oval className="oval" height="570" width="570" bottom="0" left="0">
-      <OvalSVG />
-    </Oval>
-    <HeadingCenter headline={content.header} headlineWidth="770" bottomMargin="0" />
-    <ContentCenter contentWidth="770">
-      <StatesMap />
-    </ContentCenter>
-  </Map>
+	<Map className={className}>
+		<Oval className="oval" height="570" width="570" bottom="0" left="0">
+			<OvalSVG />
+		</Oval>
+		<ContentCenter contentWidth="770">
+			<Heading size={1} template={2}>
+				{content.header}
+			</Heading>
+			<StatesMap />
+		</ContentCenter>
+	</Map>
 );
 
 export default MapSection;
--- src/pages/registered-agents.js
+++ src/pages/registered-agents.js
@@ -20,120 +20,151 @@ import Service from "../atomic/sections/learning-center-entity/registered-agents
 import LearnMore from "../atomic/sections/learning-center-entity/registered-agents/learn-more";
 import Articles from "../atomic/sections/articles";
 //Texts
-import { top, about, whatAgentDo, otherNames, need, requirements, whatHappens, faq, service, learnMore, hsForm } from "../static/learning-center-entity/registered-agents";
+import {
+	top,
+	about,
+	whatAgentDo,
+	otherNames,
+	need,
+	requirements,
+	whatHappens,
+	faq,
+	service,
+	learnMore,
+	hsForm,
+} from "../static/learning-center-entity/registered-agents";
 import styled from "styled-components";
-import {ThankYouContent} from "../components/hubspot/thank-you-modal";
+import { ThankYouContent } from "../components/hubspot/thank-you-modal";
 import RASchema from "../atomic/sections/learning-center-entity/registered-agents/schema";
 
 const RegisteredAgents = () => {
-    const [modalVisible, setModalVisible] = React.useState(false);
-    const [formSubmitted, setFormSubmitted] = React.useState(false);
-    const [modalClases, setModalClases] = React.useState(["lightbox-content"]);
+	const [modalVisible, setModalVisible] = React.useState(false);
+	const [formSubmitted, setFormSubmitted] = React.useState(false);
+	const [modalClases, setModalClases] = React.useState(["lightbox-content"]);
 
-    React.useEffect(() => {
-        if (formSubmitted) {
-            setModalClases((modalClases) => [...modalClases, "form-submitted"]);
-        }
-    }, [formSubmitted]);
+	React.useEffect(() => {
+		if (formSubmitted) {
+			setModalClases((modalClases) => [...modalClases, "form-submitted"]);
+		}
+	}, [formSubmitted]);
 
-    const popup = (e) => {
-        e.preventDefault();
+	const popup = (e) => {
+		e.preventDefault();
 
-        if (!e.target.className.includes("modal-overlay") && !e.target.className.includes("modal-close") && modalVisible) return;
+		if (
+			!e.target.className.includes("modal-overlay") &&
+			!e.target.className.includes("modal-close") &&
+			modalVisible
+		)
+			return;
 
-        setModalVisible(!modalVisible);
-        setFormSubmitted(false);
-    };
+		setModalVisible(!modalVisible);
+		setFormSubmitted(false);
+	};
 
-    const postDownload = (formData) => {
-        setModalVisible(modalVisible);
-        setFormSubmitted(true);
-    };
+	const postDownload = (formData) => {
+		setModalVisible(modalVisible);
+		setFormSubmitted(true);
+	};
 
-    return (
-        <Layout>
-            <SEO title="What is a Registered Agent? Everything You Need to Know"
-                description="Every formal business entity in the United States, whether it’s an LLC, S corp or C corp, must have a Registered Agent. Learn more." />
-            
-            <RASchema />
-           
-            <Top imageName="mr-bulb-registered-agents" imageAlt="Mrs Bulb and with checklist" ovalColor="sun">
-                <h1>{top.header}</h1>
-                <p>{top.text}</p>
-                <Buttonsbox>
-                    <Button content={top.buttons[0]} theme="primary56" arrow/>
-                </Buttonsbox>
-                <RatingRow>
-                    <CartBlock/>
-                    <RatingBlock/>
-                </RatingRow>
-            </Top>
-            <About content={about} openHSForm={popup} />
-            <WhatAgentDo content={whatAgentDo}/>
-            <OtherNames content={otherNames}/>
-            <Need content={need}/>
-            <Requirements content={requirements} openHSForm={popup} />
-            <WhatHappens content={whatHappens}/>
-            <Faq content={faq}/>
-            <Service content={service}/>
-            <LearnMore content={learnMore}/>
-            <Articles categoryId={324} />
-            <LightBoxModal visible={modalVisible} className="modal-overlay">
-                <LightBoxContent style={{ pointerEvents: "all" }} class={modalClases.join(" ")}>
-                    {!formSubmitted && <HSFormModal hs_form_id={hsForm.hs_form_id} content={hsForm} modalExit={popup} postDownloadAction={postDownload} />}
-                    {formSubmitted && <ThankYouContent modalExit={popup} />}
-                </LightBoxContent>
-            </LightBoxModal>
-        </Layout>
-    );
+	return (
+		<Layout>
+			<SEO
+				title="What is a Registered Agent? Everything You Need to Know"
+				description="Every formal business entity in the United States, whether it’s an LLC, S corp or C corp, must have a Registered Agent. Learn more."
+			/>
+
+			<RASchema />
+
+			<Top
+				imageName="mr-bulb-registered-agents"
+				imageAlt="Mrs Bulb and with checklist"
+				ovalColor="sun"
+				headlineWidth="585"
+			>
+				<h1>{top.header}</h1>
+				<Buttonsbox>
+					<Button content={top.buttons[0]} theme="primary56" arrow />
+				</Buttonsbox>
+				<RatingRow>
+					<CartBlock />
+					<RatingBlock />
+				</RatingRow>
+			</Top>
+			<About content={about} openHSForm={popup} />
+			<WhatAgentDo content={whatAgentDo} />
+			<OtherNames content={otherNames} />
+			<Need content={need} />
+			<Requirements content={requirements} openHSForm={popup} />
+			<WhatHappens content={whatHappens} />
+			<Faq content={faq} />
+			<Service content={service} />
+			<LearnMore content={learnMore} />
+			<Articles categoryId={324} />
+			<LightBoxModal visible={modalVisible} className="modal-overlay">
+				<LightBoxContent
+					style={{ pointerEvents: "all" }}
+					class={modalClases.join(" ")}
+				>
+					{!formSubmitted && (
+						<HSFormModal
+							hs_form_id={hsForm.hs_form_id}
+							content={hsForm}
+							modalExit={popup}
+							postDownloadAction={postDownload}
+						/>
+					)}
+					{formSubmitted && <ThankYouContent modalExit={popup} />}
+				</LightBoxContent>
+			</LightBoxModal>
+		</Layout>
+	);
 };
 
 const LightBoxModal = styled.div`
-  position: fixed;
-  z-index: 9999;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  background: rgba(0, 0, 0, 0.8);
-  opacity: ${(props) => (props.visible ? "1" : "0")};
-  visibility: ${(props) => (props.visible ? "visible" : "hidden")};
+	position: fixed;
+	z-index: 9999;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	background: rgba(0, 0, 0, 0.8);
+	opacity: ${(props) => (props.visible ? "1" : "0")};
+	visibility: ${(props) => (props.visible ? "visible" : "hidden")};
 `;
 
 const LightBoxContent = styled.div`
-  transition: height 0.5s, max-width 0.5s;
+	transition: height 0.5s, max-width 0.5s;
 
-  background-color: #fff;
-  width: 100%;
-  max-width: 750px;
-  position: relative;
-  //margin: 0 30px;
-  max-height: 100vh;
-  overflow-y: auto;
+	background-color: #fff;
+	width: 100%;
+	max-width: 750px;
+	position: relative;
+	//margin: 0 30px;
+	max-height: 100vh;
+	overflow-y: auto;
 
-  &.form-submitted {
-    height: 40vh;
-    max-width: 500px;
-  }
+	&.form-submitted {
+		height: 40vh;
+		max-width: 500px;
+	}
 
-  @media screen and (min-width: 769px) {
-    padding-top: 0;
-    max-width: 600px;
-    max-height: 80vh;
-  }
+	@media screen and (min-width: 769px) {
+		padding-top: 0;
+		max-width: 600px;
+		max-height: 80vh;
+	}
 
-  form {
-    padding: 25px 15px;
+	form {
+		padding: 25px 15px;
 
-    @media (min-width: 769px) {
-      padding: 50px;
-    }
-  }
+		@media (min-width: 769px) {
+			padding: 50px;
+		}
+	}
 `;
 
-
 export default RegisteredAgents;
-
