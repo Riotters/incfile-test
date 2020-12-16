a5fe1c6322d20293972d9868725752e2796477b3
Riotters <krzysztof@riotters.com>
2020-12-16 06:58:10 +0100

    Incfile brand name fixes

--- src/pages/about.js
+++ src/pages/about.js
@@ -12,38 +12,44 @@ import WhatWeDoSection from "../atomic/sections/other/about/what-we-do";
 import Rocket from "../atomic/sections/review-entity-types/c-corporation/rocket";
 
 //Texts
-import { seo, hero, whyChooseUsList, ourProcess, whatWeDo, rocket } from "../static/other/about";
+import {
+	seo,
+	hero,
+	whyChooseUsList,
+	ourProcess,
+	whatWeDo,
+	rocket,
+} from "../static/other/about";
 
 const About = () => (
-  <Layout>
-        <SEO
-            title={seo.title}
-            description={seo.desc}
-        />
+	<Layout>
+		<SEO title={seo.title} description={seo.desc} />
 
-        <Top imageName="mrs-and-mr-about-us"
-            imageAlt="about incfile"
-            ovalColor="purple1"
-            contentWidth="55"
-            contentPaddingTop="0"
-            headlineWidth="700"
-            tabletVariantClass="tablet--shopper"
-            tabletImgPosX={33}
-            tabletImgPosY={-510}
-            tabletImgScale={.8}>
-            <h1>{hero.header}</h1>
-            <p>{hero.subtitle}</p>
-            <p style={{ fontWeight: `bold` }}>{hero.subtitle2}</p>
-            <Buttonsbox>
-                <Button content={hero.button} theme="primary56" arrow />
-            </Buttonsbox>
-        </Top>
+		<Top
+			imageName="mrs-and-mr-about-us"
+			imageAlt="about Incfile"
+			ovalColor="purple1"
+			contentWidth="55"
+			contentPaddingTop="0"
+			headlineWidth="700"
+			tabletVariantClass="tablet--shopper"
+			tabletImgPosX={33}
+			tabletImgPosY={-510}
+			tabletImgScale={0.8}
+		>
+			<h1>{hero.header}</h1>
+			<p>{hero.subtitle}</p>
+			<p style={{ fontWeight: `bold` }}>{hero.subtitle2}</p>
+			<Buttonsbox>
+				<Button content={hero.button} theme="primary56" arrow />
+			</Buttonsbox>
+		</Top>
 
-        <WhyChooseUs content={whyChooseUsList} />
-        <OurProcessSection content={ourProcess} />
-        <WhatWeDoSection content={whatWeDo} />
-        <Rocket content={rocket} />
-  </Layout>
+		<WhyChooseUs content={whyChooseUsList} />
+		<OurProcessSection content={ourProcess} />
+		<WhatWeDoSection content={whatWeDo} />
+		<Rocket content={rocket} />
+	</Layout>
 );
 
 export default About;
--- src/static/research-topics/s-corporation-info/stock-par-value.js
+++ src/static/research-topics/s-corporation-info/stock-par-value.js
@@ -1,14 +1,14 @@
 export const about = {
-    links: [
-        {
-            text: `View All Topics`,
-            url: `/help-center/`,
-        },
-        {
-            text: `S Corporation Information`,
-            url: `/research-topics/s-corporation-info/`,
-        },
-    ],
-    header: `What is Stock Par Value?`,
-    text: `Par value is a nominal dollar amount given to corporate shares. It doesn’t necessarily reflect their real value, and is typically set at a low value (i.e. one dollar or one cent). The par value of a share is the minimum price at which it may be sold to shareholders, and the par value must be the same for all shares of the same class. The shares can be sold to the initial shareholders, at par value or more, but the price must be the same for each share. Not all states require a par value. Unless you specify otherwise, IncFile will authorize 1500 shares (this is due to the fact that 1500 is easily divisible by 2, 3, 4, 5, 6) with a par value of one cent, or at no par value if not required by your state.`,
-}
+	links: [
+		{
+			text: `View All Topics`,
+			url: `/help-center/`,
+		},
+		{
+			text: `S Corporation Information`,
+			url: `/research-topics/s-corporation-info/`,
+		},
+	],
+	header: `What is Stock Par Value?`,
+	text: `Par value is a nominal dollar amount given to corporate shares. It doesn’t necessarily reflect their real value, and is typically set at a low value (i.e. one dollar or one cent). The par value of a share is the minimum price at which it may be sold to shareholders, and the par value must be the same for all shares of the same class. The shares can be sold to the initial shareholders, at par value or more, but the price must be the same for each share. Not all states require a par value. Unless you specify otherwise, Incfile will authorize 1500 shares (this is due to the fact that 1500 is easily divisible by 2, 3, 4, 5, 6) with a par value of one cent, or at no par value if not required by your state.`,
+};
