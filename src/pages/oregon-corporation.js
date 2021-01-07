cc33b1b342f3ad1c1ec9d32bd1f6add189666c7e
omgxghxgf <a.brylanska@wp.pl>
2021-01-07 18:23:20 +0100

    aaaaaa

--- src/pages/oregon-corporation.js
+++ src/pages/oregon-corporation.js
@@ -166,6 +166,7 @@ const OregonCorporation = () => {
               </Paragraph>
             </PanelWrapper>
           </TabPanel>
+
           <TabPanel>
             <PanelWrapper>
               <Heading
