2c113fe2d02c6d103841294642b61512d7dbdac6
Riotters <krzysztof@riotters.com>
2021-01-07 09:47:18 +0100

    Revert "Merge pull request #270 from Riotters/robert"
    This reverts commit f918ca15f1c8aedc38c30a9bedcbe796070e8cec, reversing
    changes made to 190f46f522cbe675e02616d4913e74f41fe870bc.

--- src/pages/manage-your-company/foreign-qualification.js
+++ src/pages/manage-your-company/foreign-qualification.js
@@ -16,58 +16,52 @@ import Benefits from "../../atomic/sections/benefits";
 import Articles from "../../atomic/sections/articles";
 //Text
 import {
-  top,
-  about,
-  basics,
-  help,
-  faq,
+	top,
+	about,
+	basics,
+	help,
+	faq,
 } from "../../static/manage-your-company/foreign-qualification";
 import ProductSchema from "../../components/product-schema";
 import { servicePrice } from "../../helpers/global-variables";
 
 const ForeignQualification = () => (
-  <Layout>
-    <SEO
-      title="File Foreign LLC Qualification | Certificate of Authority"
-      description="To do business in another state, you may need to file a foreign qualification for a certificate of authority. Have Incfile file the paperwork for you.  "
-    />
+	<Layout>
+		<SEO
+			title="File Foreign LLC Qualification | Certificate of Authority"
+			description="To do business in another state, you may need to file a foreign qualification for a certificate of authority. Have Incfile file the paperwork for you.  "
+		/>
 
-    <ProductSchema
-      productUrl={`${process.env.ORDER_URL}foreign-qual.php`}
-      productName="Registered AgentForeign Qualification Filing"
-      productPrice={servicePrice.foreignQual}
-      productDescription="File the correct paperwork to legally do business in states other than your state of incorporation."
-    />
+		<ProductSchema
+			productUrl={`${process.env.ORDER_URL}foreign-qual.php`}
+			productName="Registered AgentForeign Qualification Filing"
+			productPrice={servicePrice.foreignQual}
+			productDescription="File the correct paperwork to legally do business in states other than your state of incorporation."
+		/>
 
-    <Top
-      imageName="mrs-bulb-foreign-qualification"
-      imageAlt="Filing a Foreign Qualification"
-      headlineWidth="515"
-      ovalColor="green"
-    >
-      <h1>{top.header}</h1>
-      <p>{top.text}</p>
-      <Buttonsbox>
-        <Button
-          externalLink
-          content={top.button[0]}
-          theme="primary56"
-          width="200px"
-          arrow
-        />
-      </Buttonsbox>
-      <RatingRow>
-        <CartBlock />
-        <RatingBlock />
-      </RatingRow>
-    </Top>
-    <About content={about} />
-    <Basics content={basics} />
-    <Help content={help} />
-    <Faq content={faq} />
-    <Benefits page="foreign-qualification" />
-    <Articles categoryId={309} />
-  </Layout>
+		<Top
+			imageName="mrs-bulb-foreign-qualification"
+			imageAlt="Filing a Foreign Qualification"
+			headlineWidth="515"
+			ovalColor="green"
+		>
+			<h1>{top.header}</h1>
+			<p>{top.text}</p>
+			<Buttonsbox>
+				<Button externalLink content={top.button[0]} theme="primary56" width="200px" arrow />
+			</Buttonsbox>
+			<RatingRow>
+				<CartBlock />
+				<RatingBlock />
+			</RatingRow>
+		</Top>
+		<About content={about} />
+		<Basics content={basics} />
+		<Help content={help} />
+		<Faq content={faq} />
+		<Benefits page="foreign-qualification" />
+		<Articles categoryId={309} />
+	</Layout>
 );
 
 export default ForeignQualification;
