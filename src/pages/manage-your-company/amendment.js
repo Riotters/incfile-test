75e2ab2c6228ae156667c63c17ebf1fcef983b54
Riotters <krzysztof@riotters.com>
2021-01-07 15:22:10 +0100

    Revert "Merge pull request #270 from Riotters/robert"
    This reverts commit f918ca15f1c8aedc38c30a9bedcbe796070e8cec, reversing
    changes made to 190f46f522cbe675e02616d4913e74f41fe870bc.

--- src/pages/manage-your-company/amendment.js
+++ src/pages/manage-your-company/amendment.js
@@ -16,64 +16,52 @@ import Benefits from "../../atomic/sections/benefits";
 import Articles from "../../atomic/sections/articles";
 
 //Texts
-import {
-  seo,
-  top,
-  about,
-  requirements,
-  faq,
-} from "../../static/manage-your-company/amendment";
+import { seo, top, about, requirements, faq } from "../../static/manage-your-company/amendment";
 import ProductSchema from "../../components/product-schema";
 import { servicePrice } from "../../helpers/global-variables";
 
 const Amendment = () => (
-  <Layout>
-    <SEO title={seo.title} description={seo.desc} />
-    <ProductSchema
-      productUrl={`${process.env.ORDER_URL}/amendment.php`}
-      productName="Articles of Amendment for LLCs or Corporations"
-      productPrice={servicePrice.amendment}
-      productDescription="File an official amendment to change members, your business address, authorized shares, or to alter business activities."
-    />
-    <Top
-      imageName="mr-amendment-name-change"
-      imageWidth="948"
-      imagePositionRight="50%"
-      imageAlt="Articles of Amendment for LLCs or Corporations"
-      ovalColor="orange"
-      imageMobilePosition={0}
-      tabletVariantClass="tablet--shopper"
-      tabletImgScale={1}
-      tabletImgPosX={40}
-      tabletImgPosY={-450}
-    >
-      <h1>{top.header}</h1>
-      <Buttonsbox>
-        <Button
-          externalLink
-          content={{
-            text: `Order Now`,
-            url: `${process.env.ORDER_URL}/amendment.php`,
-          }}
-          theme="primary56"
-          width="200px"
-          arrow
+    <Layout>
+        <SEO title={seo.title} description={seo.desc} />
+        <ProductSchema
+            productUrl={`${process.env.ORDER_URL}/amendment.php`}
+            productName="Articles of Amendment for LLCs or Corporations"
+            productPrice={servicePrice.amendment}
+            productDescription="File an official amendment to change members, your business address, authorized shares, or to alter business activities."
         />
-      </Buttonsbox>
-      <RatingRow>
-        <CartBlock />
-        <RatingBlock />
-      </RatingRow>
-    </Top>
-    <About content={about} />
-    <Requirements content={requirements} />
-    <Faq content={faq} />
-    <Benefits
-      bgImage="linear-gradient(to top, rgba(255, 255, 255, 0) 36%, #f2f6ff);"
-      page="amendment"
-    />
-    <Articles categoryId={1858} />
-  </Layout>
+        <Top
+            imageName="mr-amendment-name-change"
+            imageWidth="948"
+            imagePositionRight="50%"
+            imageAlt="Articles of Amendment for LLCs or Corporations"
+            ovalColor="orange"
+            imageMobilePosition={0}
+            tabletVariantClass="tablet--shopper"
+            tabletImgScale={1}
+            tabletImgPosX={40}
+            tabletImgPosY={-450}
+        >
+            <h1>{top.header}</h1>
+            <Buttonsbox>
+                <Button
+                    externalLink
+                    content={{ text: `Order Now`, url: `${process.env.ORDER_URL}/amendment.php` }}
+                    theme="primary56"
+                    width="200px"
+                    arrow
+                />
+            </Buttonsbox>
+            <RatingRow>
+                <CartBlock />
+                <RatingBlock />
+            </RatingRow>
+        </Top>
+        <About content={about} />
+        <Requirements content={requirements} />
+        <Faq content={faq} />
+        <Benefits bgImage="linear-gradient(to top, rgba(255, 255, 255, 0) 36%, #f2f6ff);" page="amendment" />
+        <Articles categoryId={1858} />
+    </Layout>
 );
 
 export default Amendment;
